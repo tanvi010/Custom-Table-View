1b31fb9c2a512f29c752ce1b164acc2888dbad15
COE-029 <coe-029@COE-029s-iMac.local>
2020-01-09 13:44:23 +0530

    first commit

---
+++ custom_table_view/MyTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MyTableViewCell.swift
+//  custom_table_view
+//
+//  Created by COE-029 on 16/12/19.
+//  Copyright © 2019 COE-029. All rights reserved.
+//
+
+import UIKit
+
+class MyTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
